3f28cb5becb971103b3cabf9fb01435adac45f3a
RaviDusarlapudi <ravi.dusarlapudi@gmail.com>
2024-03-10 10:26:48 +1300

    Create LB-Security group.tf

---
+++ LB-Security group.tf
@@ -0,0 +1,31 @@
+#LB-Security group
+
+resource "aws_security_group" "applb_sg" {
+    description =  "security group for app loadbalancer"
+    vpc_id      = aws_vpc.my_vpc.id
+    name        = "applb_sg"
+
+    ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "Allow Port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
